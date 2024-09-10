550c1f9d785947c44d222ac5ee87fe77900a2e64
Jonibek Uralov <anonymousbycreated@gmail.com>
2024-09-10 08:27:44 +0500

    deployga tayyorlandi

--- library/serializers.py
+++ library/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from library.models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('title', 'subtitle', 'author', 'isbn', 'price',)
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+
+        if not title.isalpha():
+            raise ValidationError(
+                {
+                    'status': False,
+                    'message': "Kitob sarlavhasi satr bo'lishi kerak"
+                }
+            )
+
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError(
+                {
+                'status': False,
+                'message': "Muallif va kitob sarlavhasi bir xil bo'lishi mumkin amas"
+                }
+            )
+
+        return data
--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from .views import BookListApiView
+from rest_framework.routers import SimpleRouter
+from .views import BookListApiView, BookDetailApiView, BookUpdateApiView, BookDeleteApiView, \
+     BookListCreateApiView, BookUpdateDeleteApiView, BookViewSet
+
+router = SimpleRouter()
+router.register('books', BookViewSet, basename='books')
 
 urlpatterns = [
-    path('', BookListApiView.as_view())
+    # path('books/', BookListApiView.as_view()),
+    # path('booklistcreate/', BookListCreateApiView.as_view()),
+    # path('bookupdatedelete/<int:pk>/', BookUpdateDeleteApiView.as_view()),
+    # # path('books/create/', BookCreateApiview.as_view()),
+    # path('books/<int:pk>/', BookDetailApiView.as_view()),
+    # path('books/<int:pk>/update/', BookUpdateApiView.as_view()),
+    # path('books/<int:pk>/delete/', BookDeleteApiView.as_view()),
+    # path('books/', book_list_view)
 ]
+
+urlpatterns = urlpatterns + router.urls
